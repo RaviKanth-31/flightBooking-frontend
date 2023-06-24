a13d40a97d8a858da4befbbcbf375ed1b4327cda
RaviKanth-31 <74865487+RaviKanth-31@users.noreply.github.com>
2023-06-24 20:02:37 +0530

    Update Login.jsx

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -26,7 +26,7 @@ const Login = () => {
     e.preventDefault();
     dispatch({ type: "LOGIN_START" });
     try {
-      const res = await axios.post("/auth/login", credentials);
+      const res = await axios.post("https://flightbooking-backend.onrender.com/auth/login", credentials);
       dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
       if(res.data.isAdmin){
         
